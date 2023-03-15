5f56b9980b06633239ca279c6eff7bac3467437b
eijjeka <eijjeka@gmail.com>
2023-03-15 02:51:07 +0200

    merge

---
+++ build/static/js/movies-view.56d3a996.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[163],{6222:function(){},8497:function(e,t,n){"use strict";n.r(t),n.d(t,{default:function(){return c.a}});var u=n(6222),c=n.n(u)}}]);
