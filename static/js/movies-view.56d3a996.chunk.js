cc4492c6b65711d481290ad502c3b69cf6c59fe9
eijjeka <eijjeka@gmail.com>
2023-03-19 03:30:34 +0200

    Updates

--- static/js/movies-view.56d3a996.chunk.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[163],{6222:function(){},8497:function(e,t,n){"use strict";n.r(t),n.d(t,{default:function(){return c.a}});var u=n(6222),c=n.n(u)}}]);
