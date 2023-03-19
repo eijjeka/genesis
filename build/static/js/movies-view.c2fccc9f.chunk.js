fae2f6954d1edcb0bc17d3b85792600bea948f05
eijjeka <eijjeka@gmail.com>
2023-03-19 03:35:52 +0200

    add config

--- build/static/js/movies-view.c2fccc9f.chunk.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkgenesis=self.webpackChunkgenesis||[]).push([[163],{6222:function(){},8497:function(n,e,s){"use strict";s.r(e),s.d(e,{default:function(){return t.a}});var u=s(6222),t=s.n(u)}}]);
